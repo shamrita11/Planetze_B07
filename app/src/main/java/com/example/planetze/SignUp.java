8dbe9c0b69f18abffccbe3bb2dc0099c18419311
Shahad Osman <shahad.osman@mail.utoronto.ca>
2024-11-18 10:41:08 -0500

    fix: publicly leaked secret

--- app/src/main/java/com/example/planetze/SignUp.java
+++ app/src/main/java/com/example/planetze/SignUp.java
@@ -46,6 +46,7 @@ public class SignUp extends AppCompatActivity {
         }
     }
 
+    // comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
