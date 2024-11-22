5f2ac73cb5288acdfa3cbab2a5bb82e9db81ee0c
Shahad Osman <shahad.osman@mail.utoronto.ca>
2024-11-22 13:37:45 -0500

    feat: retrieve user name & email from database and display it

--- app/src/main/java/com/example/planetze/Account.java
+++ app/src/main/java/com/example/planetze/Account.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Account extends AppCompatActivity {
 
     private Button buttonLogOut, buttonEditSurvey;
     private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase db;
+    private DatabaseReference ref;
+    private TextView tvName, tvEmail;
+    private String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +39,30 @@ public class Account extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account);
         mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        db = FirebaseDatabase.getInstance();
+        ref = db.getReference("users");
         buttonLogOut = findViewById(R.id.btn_logout);
         buttonEditSurvey = findViewById(R.id.btn_editsurvey);
+        tvName = findViewById(R.id.userName);
+        tvEmail = findViewById(R.id.userEmail);
+        uid = user.getUid();
+
+        // display user's info
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child(uid).child("name").getValue(String.class);
+                String email = snapshot.child(uid).child("email").getValue(String.class);
+                tvName.setText(name);
+                tvEmail.setText(email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Account.this, "Failed to retrieve data.", Toast.LENGTH_LONG).show();
+            }
+        });
 
         buttonLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
